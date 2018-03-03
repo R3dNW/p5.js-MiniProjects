883efa9ca0f40124b3c649d1b6caa245d9ad60ff
R3dNW <R3dNW@users.noreply.github.com>
2018-03-03 02:28:19 +0000

    Improved the StringCircle code to make it more adaptable.

--- Projects/StringCircle/sketch.js
+++ Projects/StringCircle/sketch.js
@@ -1,13 +1,4 @@
-var resolution = 1024;
-var rotations = 6;
-var radius_ratio = 1/5;
-var fac = 0;
-
-var col_e = [128, 0, 255];
-var col_m = [0, 128, 255];
-
-
-var radius;
+var stringCircle;
 
 function preload(){
 
@@ -15,55 +6,12 @@ function preload(){
 
 function setup(){
 	createCanvas(window.innerWidth, window.innerHeight);
-}
 
-function getCoord(i){
-	var angle = i / resolution * rotations * TWO_PI;
-	var distortedRadius = radius + noise((i/(resolution/rotations)) * 4, fac) * radius * 1.5;
-
-	var x = distortedRadius * cos(angle + fac);
-	var y = distortedRadius * sin(angle + fac);
-
-	return [x, y]
+	stringCircle = new string(1024, 5.32);
 }
 
 function draw(){
-	background(0);
-	push();
-	
-	translate(width/2, height/2);
-
-	noFill();
-	stroke(255);
-	strokeWeight(5);
-
-
-    radius = radius_ratio*min(width, height);
-
-	var prev_l = getCoord(0);
-	var prev_r = getCoord(0);
-
-	for (var i = 1; i < resolution/2 + 100; i++) {
-		var a = -((4*1)/(resolution*resolution))*(i-resolution/2)*(i-resolution/2)+1;
-		
-		r = map(a, 0, 1, col_e[0], col_m[0])
-		g = map(a, 0, 1, col_e[1], col_m[1])
-		b = map(a, 0, 1, col_e[2], col_m[2])
-
-		stroke(r*a, g*a, b*a);
-
-		coord = getCoord(i);
-		line(prev_l[0], prev_l[1], coord[0], coord[1]);
-		prev_l = coord;
-
-		coord = getCoord(resolution - i);
-		line(prev_r[0], prev_r[1], coord[0], coord[1]);
-		prev_r = coord;
-	}
-
-	pop();
-
-	fac += 0.01;
+	stringCircle.draw();
 }
 
 window.onresize = function() {
---
+++ Projects/StringCircle/string.js
@@ -0,0 +1,87 @@
+function string(resolution, rotations, color_mid=null, color_ends=null, spiral=false){
+	this.resolution = resolution;
+	this.rotations = rotations;
+
+	if (color_mid){
+		this.fixedColor = true;
+		this.color_mid = color_mid;
+		if (this.color_ends){
+			this.color_ends = color_ends;
+		}
+		else{
+			this.color_ends = color_mid;
+		}
+	}
+	else{
+		this.fixedColor = false;
+	}
+
+	this.radius_screenSize_ratio = 1/5;
+	this.radius;
+	this.timeScale = 0.05 * (0.001);
+
+	this.spiral = spiral;
+}
+
+
+string.prototype.distortRadius = function(radius, angle, t){
+	return this.radius + noise(angle * 0.5, t*1.5) * this.radius * 1.5; 
+}
+
+string.prototype.getCoord = function(i, t){
+	var angle = i / this.resolution * this.rotations * TWO_PI;
+	var r = this.distortRadius(this.radius, angle, t);
+
+	var x = r * cos(angle + t);
+	var y = r * sin(angle + t);
+
+	return [x, y];
+}
+
+string.prototype.getAlpha = function(i, degree=1, scale=1){
+	p = 2*round(degree);
+	return -((pow(2, p)*scale)/pow(this.resolution, p))*pow(i-this.resolution/2, p)+scale;
+}
+
+string.prototype.draw = function(){
+	background(0);
+	push();
+	
+	t = Date.now() * this.timeScale;
+
+	translate(width/2, height/2);
+
+	if (!this.fixedColor){
+		colorMode(HSB);
+		this.color_mid =  color((t * 10 + 60) % 360, 255, 255);
+		this.color_ends = color((t * 10     ) % 360, 255, 255);
+		colorMode(RGB);
+	}
+
+	strokeWeight(5);
+
+    this.radius = this.radius_screenSize_ratio*min(width, height);
+
+	var prev_l = this.getCoord(0, t);
+	var prev_r = this.getCoord(this.resolution - 1, t);
+
+	for (var i = 0; i < this.resolution/2 + 1; i++) {
+		var a = this.getAlpha(i);
+
+		c = lerpColor(this.color_ends, this.color_mid, a);
+		c.setRed(c._getRed() * a);
+		c.setGreen(c._getGreen() * a);
+		c.setBlue(c._getBlue() * a);
+		stroke(c);
+
+		coord = this.getCoord(i, t);
+		line(prev_l[0], prev_l[1], coord[0], coord[1]);
+		prev_l = coord;
+
+		coord = this.getCoord(this.resolution - i, t);
+		line(prev_r[0], prev_r[1], coord[0], coord[1]);
+		prev_r = coord;
+	}
+
+	pop();
+}
